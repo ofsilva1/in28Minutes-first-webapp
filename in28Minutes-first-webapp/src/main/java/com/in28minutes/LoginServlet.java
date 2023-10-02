0534ac917334b9cbc1bcd54b9f5e035fd9d38adf
Omar Silva <abettermanep@gmail.com>
2023-10-01 21:32:50 -0600

    SpringMvcStepByStep step06

--- in28Minutes-first-webapp/src/main/java/com/in28minutes/LoginServlet.java
+++ in28Minutes-first-webapp/src/main/java/com/in28minutes/LoginServlet.java
@@ -13,10 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.setAttribute("name", request.getParameter("name"));
-		request.setAttribute("password", request.getParameter("password"));
-		
-		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
 	}
 }
