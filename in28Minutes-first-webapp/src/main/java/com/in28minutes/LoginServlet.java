01aa92be9b544f681d3acc94b0a85b5cf8849123
Omar Silva <abettermanep@gmail.com>
2023-10-01 22:02:11 -0600

    SpringMvcStepByStep step07

--- in28Minutes-first-webapp/src/main/java/com/in28minutes/LoginServlet.java
+++ in28Minutes-first-webapp/src/main/java/com/in28minutes/LoginServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
+	
+	private UserValidationService service = new UserValidationService();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 				
@@ -19,7 +21,18 @@ public class LoginServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.setAttribute("name", request.getParameter("name"));
-		request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		
+		boolean isUserValid = service.isUserValid(name, password);
+		
+		if (isUserValid) {
+			request.setAttribute("name", name);
+			request.setAttribute("password", password);
+			request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+		} else {
+			request.setAttribute("errorMessage", "Invalid Credentials!");
+			request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		}
 	}
 }
