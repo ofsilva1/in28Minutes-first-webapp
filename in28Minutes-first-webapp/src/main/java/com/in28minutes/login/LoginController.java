d7ed44fc8814fc0c7b53762c05c4996f95669f2a
Omar Silva <abettermanep@gmail.com>
2023-10-10 12:36:13 -0600

    Spring MvcStepByStep step16

--- in28Minutes-first-webapp/src/main/java/com/in28minutes/login/LoginController.java
+++ in28Minutes-first-webapp/src/main/java/com/in28minutes/login/LoginController.java
@@ -1,4 +1,4 @@
-package com.in28minutes.springmvc;
+package com.in28minutes.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.in28minutes.login.LoginService;
+
 @Controller
 public class LoginController {
+	
+	LoginService service = new LoginService();
 	
 	@RequestMapping(value="/login", method= RequestMethod.GET)
 	public String showLoginPage() {
@@ -17,6 +21,12 @@ public class LoginController {
 	@RequestMapping(value="/login", method= RequestMethod.POST)
 	public String handleLoginRequest(@RequestParam String name,
 			@RequestParam String password, ModelMap model) {
+		if (!service.validateUser(name, password)) {
+			model.put("errorMessage", "Invalid Credentials");
+			return "login";
+			
+		}
+		
 		model.put("name", name);
 		model.put("password", password);
 		return "welcome";
--- in28Minutes-first-webapp/src/main/webapp/WEB-INF/views/login.jsp
+++ in28Minutes-first-webapp/src/main/webapp/WEB-INF/views/login.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 <p><font color="red">${errorMessage}</font></p>
-<form action="/spring-mvc/login" method="post">
+<form action="/login" method="post">
 Name: <input type="text" name="name"/> Password: <input type="password" name="password"/> <input type="submit" value="Login"/>
 </form>
 </body>
