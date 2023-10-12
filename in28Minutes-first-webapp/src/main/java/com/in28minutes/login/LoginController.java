d6aac395b51d5a76c17cb2e131cb0a5db3566f6e
Omar Silva <abettermanep@gmail.com>
2023-10-12 09:29:20 -0600

    Spring MvcStepByStep step17

--- in28Minutes-first-webapp/src/main/java/com/in28minutes/login/LoginController.java
+++ in28Minutes-first-webapp/src/main/java/com/in28minutes/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,8 @@ import com.in28minutes.login.LoginService;
 @Controller
 public class LoginController {
 	
-	LoginService service = new LoginService();
+	@Autowired
+	LoginService service;
 	
 	@RequestMapping(value="/login", method= RequestMethod.GET)
 	public String showLoginPage() {
